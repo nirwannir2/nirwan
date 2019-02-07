5ba02344e6ad101d86d7d520ed3ef7a692163144
nirwannir2 <47424143+nirwannir2@users.noreply.github.com>
2019-02-07 22:34:33 +0700

    Add files via upload

---
+++ nirwan.php
@@ -0,0 +1,19 @@
+<?php
+
+$kata="Variabel Kecil";
+$Kata="Variabel Besar";
+$angka1="123";
+$angka2=456;
+$angka3=789;
+
+echo "$angka1 <br>";
+echo $angka2. "<br>";
+echo $angka3;
+
+
+echo $angka1."<br>".angka2."<br>".$angka3;
+echo "$angka1<br>$angka2<br>$angka3";
+
+
+
+?>
